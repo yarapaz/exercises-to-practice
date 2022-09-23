05f76b8366deb2aabca212056a6bfbb8600258bc
Yara <yara.gordillo@gmail.com>
2022-09-23 20:04:03 +0200

    Ejercicios de hoy

---
+++ mis-ideas-spotify/one.js
@@ -0,0 +1,7 @@
+function Hola () {
+    document.getElementById("navigation").classList.toggle("show");
+}
+// /* When the user clicks on the button,
+// toggle between hiding and showing the dropdown content */
+// Va al documento, coge el elemento por ese ID o clase y hace que aparezca y desaparezca lo contenido en la clase show
+// que lo que hace es cambiar el display a block
