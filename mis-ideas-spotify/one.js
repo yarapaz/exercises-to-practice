5bb235e8efb884bac34b17c4ddc97e3072ea74b6
Yara <yara.gordillo@gmail.com>
2022-09-25 18:58:14 +0200

    Actualizaciones domingo

--- mis-ideas-spotify/one.js
+++ mis-ideas-spotify/one.js
@@ -3,5 +3,5 @@ function Hola () {
 }
 // /* When the user clicks on the button,
 // toggle between hiding and showing the dropdown content */
-// Va al documento, coge el elemento por ese ID o clase y hace que aparezca y desaparezca lo contenido en la clase show
+// Va al documento, coge el elemento por ese ID o clase y hace que aparezca y desaparezca lo contenido en la clase show. Como que le añade la clase SHOW
 // que lo que hace es cambiar el display a block
