68852158d236342bbd95f77ed991b8062962d277
Yara <yara.gordillo@gmail.com>
2022-10-20 21:11:56 +0200

    exercises improved and updated

--- modulo-2/modulo-2-leccion-04-eventos/modulo-2-leccion-04-ejercicio-04-dame-ipsum-YaraPaz/main.js
+++ modulo-2/modulo-2-leccion-04-eventos/modulo-2-leccion-04-ejercicio-04-dame-ipsum-YaraPaz/main.js
@@ -1,11 +1,8 @@
 'use strict';
 
 const ipsumText = document.querySelector('.text');
+const lorem = ipsumText.innerHTML;
 
 ipsumText.addEventListener('click', () => {
-  console.log(
-    console.log(
-      (ipsumText.innerHTML = ipsumText.innerHTML + ipsumText.innerHTML)
-    )
-  );
+  ipsumText.innerHTML += `${lorem}`;
 });
