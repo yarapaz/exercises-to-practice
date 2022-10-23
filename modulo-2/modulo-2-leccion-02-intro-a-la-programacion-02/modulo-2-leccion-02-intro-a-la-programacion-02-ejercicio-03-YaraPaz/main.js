610ee7585304733e25827ecbee3577b2aa4fac4e
Yara <yara.gordillo@gmail.com>
2022-10-23 22:41:06 +0200

    some changes

--- modulo-2/modulo-2-leccion-02-intro-a-la-programacion-02/modulo-2-leccion-02-intro-a-la-programacion-02-ejercicio-03-YaraPaz/main.js
+++ modulo-2/modulo-2-leccion-02-intro-a-la-programacion-02/modulo-2-leccion-02-intro-a-la-programacion-02-ejercicio-03-YaraPaz/main.js
@@ -1,14 +1,14 @@
-"use strict";
+'use strict';
 
-let partnerName = "Laura";
-let partnerSurname = "Valdenebro";
+let partnerName = 'Laura';
+let partnerSurname = 'Valdenebro';
 
-const message = document.querySelector(".parr");
+const message = document.querySelector('.parr');
 message.innerHTML =
   `The name of my partner is ${partnerName} ${partnerSurname}` +
-  " " +
-  "and its compound of" +
-  " " +
+  ' ' +
+  'and its compound of' +
+  ' ' +
   (partnerName.length + partnerSurname.length) +
-  " " +
-  "characters";
+  ' ' +
+  'characters';
