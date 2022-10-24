6b96e37277b0c15f2cc5609b568da5ac3b112e45
Yara <yara.gordillo@gmail.com>
2022-10-24 22:10:35 +0200

    new functions exercises

--- modulo-2/modulo-2-leccion-05.1-funciones/modulo-2-leccion-5.1-ejercicio-02-funcion-media/main.js
+++ modulo-2/modulo-2-leccion-05.1-funciones/modulo-2-leccion-5.1-ejercicio-02-funcion-media/main.js
@@ -4,5 +4,9 @@ function average(a, b, c, d) {
   return (a + b + c + d) / 4;
 }
 
+// const average = (a, b, c, d) => {
+//   return (a + b + c + d) / 4;
+// }
+
 const result = average(4, 4, 4, 4);
 console.log(result);
