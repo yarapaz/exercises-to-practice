f84610575b70c6304ec9edcf573eb2e2d62c9ee9
Yara <yara.gordillo@gmail.com>
2022-11-06 19:59:31 +0100

    refactor lesson 13 exercises

--- modulo-2/modulo-2-leccion-13-peticiones-al-servidor/modulo-2-lección-13-ejercicio-06 YaraPaz/main.js
+++ modulo-2/modulo-2-leccion-13-peticiones-al-servidor/modulo-2-lección-13-ejercicio-06 YaraPaz/main.js
@@ -13,7 +13,7 @@ function getBreedImages() {
   Promise.all(promises).then((responses) => {
     //ejecutamos promise.all pasandole como parametro el array de promesas. Una vez todas las promesas de este array se hayan cumplido se ejecutará el THEN posterior a la que le llegan todos los resultados de este array en su parametro responses. Este responses será otro array nuevo con las respuestas
     for (let i = 0; i < responses.length; i++) {
-      const img = document.querySelector('.dog' + (i + 1));
+      const img = document.querySelector('.dog' + (i + 1)); //forma de llamar a la clase de forma genérica basándonos en su posición del array
       img.src = responses[i].message;
     }
   });
