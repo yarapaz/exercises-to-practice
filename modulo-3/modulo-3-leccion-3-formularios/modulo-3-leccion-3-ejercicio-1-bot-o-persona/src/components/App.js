48124942914ec7365e307857b407f382c7bf32b4
Yara <yara.gordillo@gmail.com>
2022-11-22 18:03:58 +0100

    change code

--- modulo-3/modulo-3-leccion-3-formularios/modulo-3-leccion-3-ejercicio-1-bot-o-persona/src/components/App.js
+++ modulo-3/modulo-3-leccion-3-formularios/modulo-3-leccion-3-ejercicio-1-bot-o-persona/src/components/App.js
@@ -18,15 +18,29 @@ function App() {
   };
 
   const renderMessage = () => {
-    if (
-      ingredients.patatas === true &&
-      ingredients.huevos === true &&
-      ingredients.cebolla === true
-    ) {
+    if (ingredients.patatas && ingredients.huevos && ingredients.cebolla) {
       return 'Eres una persona concebollista';
     } else {
       return 'Eres un robot sin paladar';
     }
+  };
+
+  const handleAll = () => {
+    ingredients.macarrones === true;
+    ingredients.patatas === true;
+    ingredients.nueces === true;
+    ingredients.huevos === true;
+    ingredients.cebolla === true;
+    ingredients.cerveza === true;
+  };
+
+  const handleReset = () => {
+    ingredients.macarrones === false;
+    ingredients.patatas === false;
+    ingredients.nueces === false;
+    ingredients.huevos === false;
+    ingredients.cebolla === false;
+    ingredients.cerveza === false;
   };
 
   return (
@@ -38,19 +52,52 @@ function App() {
           type='checkbox'
           name='macarrones'
           id=''
+          value={ingredients.macarrones}
           onChange={handleChange}
         />
         <label htmlFor=''>Patatas</label>
-        <input type='checkbox' name='patatas' id='' onChange={handleChange} />
+        <input
+          type='checkbox'
+          name='patatas'
+          id=''
+          value={ingredients.patatas}
+          onChange={handleChange}
+        />
         <label htmlFor=''>Nueces</label>
-        <input type='checkbox' name='nueces' id='' onChange={handleChange} />
+        <input
+          type='checkbox'
+          name='nueces'
+          id=''
+          value={ingredients.nueces}
+          onChange={handleChange}
+        />
         <label htmlFor=''>Huevos</label>
-        <input type='checkbox' name='huevos' id='' onChange={handleChange} />
+        <input
+          type='checkbox'
+          name='huevos'
+          id=''
+          value={ingredients.huevos}
+          onChange={handleChange}
+        />
         <label htmlFor=''>Cebolla</label>
-        <input type='checkbox' name='cebolla' id='' onChange={handleChange} />
+        <input
+          type='checkbox'
+          name='cebolla'
+          id=''
+          value={ingredients.cebolla}
+          onChange={handleChange}
+        />
         <label htmlFor=''>Cerveza</label>
-        <input type='checkbox' name='cerveza' id='' onChange={handleChange} />
+        <input
+          type='checkbox'
+          name='cerveza'
+          id=''
+          value={ingredients.cerveza}
+          onChange={handleChange}
+        />
       </form>
+      <button onChange={handleAll}>Marcar todos</button>
+      <button onChange={handleReset}>Desmarcar todos</button>
       <p>{renderMessage()}</p>
     </div>
   );
