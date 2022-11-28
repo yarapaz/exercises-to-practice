c8c922433f3652add8096d5ad1428d2add15bc3f
Yara <yara.gordillo@gmail.com>
2022-11-28 21:09:08 +0100

    module 3 lesson 6

---
+++ modulo-3/modulo-3-leccion-6-componentes-y-props/src/components/App.js
@@ -0,0 +1,35 @@
+// Fichero src/components/App.js
+
+import { useState } from 'react';
+import Preview from '../components/Preview';
+
+const App = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+
+  console.log(
+    `Se renderiza el componente App con el nombre ${name} y el email ${email}`
+  );
+
+  const handleName = (ev) => {
+    setName(ev.target.value);
+  };
+
+  const handleEmail = (ev) => {
+    setEmail(ev.target.value);
+  };
+
+  return (
+    <div>
+      <form>
+        <label htmlFor='name'>Escribe tu nombre</label>
+        <input id='name' value={name} onChange={handleName} />
+        <label htmlFor='email'>Escribe tu email</label>
+        <input id='email' value={email} onChange={handleEmail} />
+      </form>
+      <Preview name={name} email={email} />
+    </div>
+  );
+};
+
+export default App;
---
+++ modulo-3/modulo-3-leccion-6-componentes-y-props/src/components/Preview.js
@@ -0,0 +1,13 @@
+// Fichero src/components/Preview.js
+
+const Preview = (props) => {
+  console.log('Renderizando el componente Preview con las props', props);
+  return (
+    <section>
+      <p>Tu nombre es {props.name}</p>
+      <p>Tu email es {props.email}</p>
+    </section>
+  );
+};
+
+export default Preview;
